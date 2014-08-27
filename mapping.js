9612ebe72bc62e2bd46b0807fd1f026485748de4
Greg MacWilliam <gmacwill77@gmail.com>
2014-08-26 23:22:16 -0400

    Edits to inn.

--- mapping.js
+++ mapping.js
@@ -96,7 +96,7 @@ module.exports.omitIndices = {
   'cliff': [47],
   'global': [120, 194, 196, 197, 200, 607, 608, 609],
   'harbor': [487, 517],
-  'inn': [23, 187, 356, 405],
+  'inn': [23, 187, 356, 406],
   'treehouse': [162],
   'trelawney': [20]
 };
